1a9235c92fa4d2f7f839b9598560cfc7713e6567
James Morris <james@jmoz.co.uk>
2012-11-13 17:45:47 +0000

    converted to listview

--- core/views.py
+++ core/views.py
@@ -3,10 +3,6 @@ from core.models import Post
 from django.views.generic.edit import CreateView, UpdateView
 from django.core.urlresolvers import reverse
 
-def home(request):
-	posts = Post.objects.all().order_by('-date_created')[:10]
-	return render_to_response('core/index.html', {'posts': posts})
-
 class PostCreate(CreateView):
 	model = Post
 	def get_success_url(self):
--- urls.py
+++ urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls.defaults import *
 from core.views import PostCreate
+from core.models import Post
+from django.views.generic import ListView
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     url(r'^post/add/$', PostCreate.as_view(), name='post_add'),
-    url('^$', 'core.views.home', name='home'),
+    url('^$', 
+    	ListView.as_view(
+    		queryset=Post.objects.all().order_by('-date_created')[:10],
+    		context_object_name='posts',
+    		template_name='core/index.html'), 
+    	name='home'),
 )
