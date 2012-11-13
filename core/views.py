468947acee1c23706298512732faf624a09e7d5e
James Morris <james@jmoz.co.uk>
2012-11-13 11:56:08 +0000

    10 posts desc

--- core/views.py
+++ core/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render_to_response
 from core.models import Post
 
 def home(request):
-	posts = Post.objects.all()
-	print posts
+	posts = Post.objects.all().order_by('-date_created')[:10]
 	return render_to_response('core/index.html', {'posts': posts})
