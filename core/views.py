8f4601b910e2149b37ff3501a624db30c0f19001
James Morris <james@jmoz.co.uk>
2012-11-15 01:02:36 +0000

    added tagged list view

--- core/views.py
+++ core/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render_to_response
 from core.models import Post
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 from django.core.urlresolvers import reverse
 
 class PostCreate(CreateView):
 	model = Post
 	def get_success_url(self):
 		return reverse('home')
+
+
+class TaggedList(ListView):
+	context_object_name = 'posts'
+	template_name = 'core/index.html'
+	def get_queryset(self):
+		tags = self.kwargs['tag']
+		return Post.objects.filter(tags=tags).order_by('-date_created')[:10]
--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from core.views import PostCreate
+from core.views import PostCreate, TaggedList
 from core.models import Post
 from django.views.generic import ListView
 from django.contrib import admin
@@ -12,6 +12,9 @@ urlpatterns = patterns('',
 	(r'^admin/', include(admin.site.urls)),
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     url(r'^post/add/$', PostCreate.as_view(), name='post_add'),
+    url(r'^tagged/(?P<tag>\w+)/$', 
+    	TaggedList.as_view(), 
+    	name='tagged'),
     url('^$', 
     	ListView.as_view(
     		queryset=Post.objects.all().order_by('-date_created')[:10],
